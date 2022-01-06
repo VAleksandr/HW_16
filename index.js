5386f27faf2a10d5e46c673ba8d35577b27707e6
VAleksandr <vlasovgomel@gmail.com>
2022-01-06 16:52:28 +0300

    changes index.js

--- index.js
+++ index.js
@@ -1,23 +1,48 @@
 const userName = document.getElementById('Username');
 const passWord = document.getElementById('Password');
 const btn = document.getElementById('Btn');
-const users = [
-  {
-    name: 'Sasha',
-    sign: '12345'
-  },
-  {
-    name: 'Pasha',
-    sign: '54321'
-  }
-];
+const btn_reg = document.getElementById('Btn_reg');
 
 const authorizationFun = () => {
-  users.find(({name, sign}) => name === userName.value && sign === passWord.value)?
-    window.location = 'calculator.html':
-    alert ('Error. Login or password is incorrect.'); 
+
+  if (localStorage.getItem('user') === null) {
+    alert ('Make registration..');
+  } else {
+    users = JSON.parse(localStorage.getItem('user'));
+    users.find(({name, sign}) => name === userName.value && sign === passWord.value)?
+      window.location = 'calculator.html':
+      alert ('Error. Login or password is incorrect or make registration.'); 
+  }
+}
+
+const registration = () => {
+  let users = [];
+  const user = {
+    name: userName.value,
+    sign: passWord.value
+  };
+  
+  if (localStorage.getItem('user') === null){
+    users.push(user);
+    localStorage.setItem('user', JSON.stringify(users));
+    alert ('Registration completed successfully.');  
+  } else {
+    users = JSON.parse(localStorage.getItem('user'));
+    console.log(users);
+    if (users.find(({name, sign}) => name === userName.value)) {
+      alert ('Enter another username.')
+    } else {
+      users.push(user);
+      localStorage.setItem('user', JSON.stringify(users)); 
+      alert ('Registration completed successfully.');
+    }
+  }
 }
 
 btn.onclick = () => {
   authorizationFun();
+}
+
+btn_reg.onclick = () => {
+  registration();
 }
